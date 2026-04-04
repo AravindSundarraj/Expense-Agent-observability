d479e155d774418473c206de16860b3588c4e016
Aravind Sundarraj <aravind.ssr@gmail.com>
2026-04-04 15:01:50 -0400

    commit - 9

--- agents/expense_agent.py
+++ agents/expense_agent.py
@@ -2,15 +2,41 @@ from opentelemetry import trace
 from openinference.semconv.trace import SpanAttributes, OpenInferenceSpanKindValues
 
 from tools.expense_classifier_tool import classify_expense
+from models.llm_client import LLMClient
+from models.prompts import EXPENSE_SYSTEM_PROMPT, EXPENSE_USER_PROMPT_TEMPLATE
+from opentelemetry.trace import Status, StatusCode
 
 # Get tracer
 tracer = trace.get_tracer("expense_agent_tracer")
 
+def extract_expenses(user_input: str):
+
+    return [
+        {"item": "Uber", "amount": 120},
+        {"item": "Food", "amount": 50}
+    ]
+
+def run_expense_llm(user_input, classified_items, summary):
+
+    llm = LLMClient()
+
+    user_prompt = EXPENSE_USER_PROMPT_TEMPLATE.format(
+        user_input=user_input,
+        classified_items=classified_items,
+        summary=summary
+    )
+
+    return llm.chat(
+        system_prompt=EXPENSE_SYSTEM_PROMPT,
+        user_prompt=user_prompt
+    )
 
 def run_expense_agent(user_input: str):
     """
     Entry point for Expense Agent
+
     """
+    
 
     # 🔍 ROOT SPAN (TRACE)
     with tracer.start_as_current_span("expense_agent_chain-span") as root_span:
@@ -84,19 +110,31 @@ def run_expense_agent(user_input: str):
             # Observability metadata
             span.set_attribute("categories.count", len(total_by_category))
             span.set_attribute("items.count", len(classified))
+        with tracer.start_as_current_span(
+        "expense_llm_generation") as llm_span:
+    
+
+            llm_span.set_attribute(
+            SpanAttributes.OPENINFERENCE_SPAN_KIND,
+            OpenInferenceSpanKindValues.LLM.value)
+    
+            try:
+                llm_output = run_expense_llm(user_input, classified, total_by_category)
+                llm_span.set_attribute(SpanAttributes.OUTPUT_VALUE, llm_output)
+                llm_span.set_status(Status(StatusCode.OK))
+                llm_span.set_attribute("llm.model", "gpt-4o-mini")
+                llm_span.set_attribute("llm.provider", "openai")
+            except Exception as e:
+                llm_span.set_status(Status(StatusCode.ERROR, str(e)))
+                raise e
+            
 
 
         # For now, just return input
-        return final_output
+    return llm_output
 
 '''
 The expense extractor is isolated in a separate span to clearly capture and measure 
 the input parsing step independently from other agent logic, enabling visibility 
 into how raw user input is transformed into structured expense data.
 '''    
-def extract_expenses(user_input: str):
-
-    return [
-        {"item": "Uber", "amount": 120},
-        {"item": "Food", "amount": 50}
-    ]
